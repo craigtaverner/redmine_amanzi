ba06d40b80575eba13be7613aa7af39b04ca93c8
Craig Taverner <craig@amanzi.com>
2013-04-19 16:47:54 +0200

    Dependencies for Raphael charts used in new raphael macro

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -3,8 +3,13 @@ module Redmine
     module Textile
       module Helper
         def heads_for_wiki_formatter
-          stylesheet_link_tag 'jstoolbar'
-          javascript_include_tag 'raphael', :plugin => 'redmine_amanzi'
+          [
+            stylesheet_link_tag('jstoolbar'),
+            stylesheet_link_tag('css-tooltips', :plugin => 'redmine_amanzi'),
+            stylesheet_link_tag('inline', :plugin => 'redmine_amanzi'),
+            javascript_include_tag('raphael', :plugin => 'redmine_amanzi')
+            #javascript_include_tag('jquery-1.8.2.min.js', :plugin => 'redmine_amanzi')
+          ].join("\n")
         end
       end
     end
